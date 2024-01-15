49947ba272248ec0fcf47488487224d214459643
Joona Sääskilahti <saaskijoona@gmail.com>
2024-01-15 08:22:16 +0200

    randomExample

---
+++ randomExample/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //sovellus tulostaa satunnaisluvun
+    //väliltä 1-10
+    srand(time(0));
+    int myRandom = rand()%10 + 1;
+    cout << "Satunnaisluku = "<< myRandom << endl;
+    return 0;
+}
