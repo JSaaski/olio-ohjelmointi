53e9ea9e88da587186c7b8851b83d21254c0be9d
Joona Sääskilahti <saaskijoona@gmail.com>
2024-01-16 20:46:31 +0200

    classExample1 aloitettu

--- classExample1/main.cpp
+++ classExample1/main.cpp
@@ -1,9 +1,22 @@
+#include "student.h"
+
 #include <iostream>
 
 using namespace std;
+//esimerkki Student luokasta, jolla
+//private tyyppiset jäsenmuuttujat fname, lname
+//ja niille public tyyppiset Getter ja Setter metodit
+//sekä public tyyppinen metodi studentInfo, joka tulostaa
+//tiedot
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    //luodaan Student luokastaolio pinoon
+    Student objectStudent1;
+    objectStudent1.setFname("Teppo");
+    objectStudent1.setLname("Testi");
+    objectStudent1.studentInfo();
+
     return 0;
 }
+
